40011f2e793029b4616236680702733eac6c52b3
Lerkus <Sebastian-Maag@gmx.de>
2017-12-09 23:46:39 +0100

    Music now stays for ever!

---
+++ MichaelBaySimulator/Assets/Scripts/MusicSingelton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSingelton : MonoBehaviour {
+
+    public GameObject _MusicPrefab;
+    private readonly string _MusicCloneObjectName = "Musik(Clone)";
+	
+	public void Start () {
+		if(GameObject.Find(_MusicCloneObjectName) == null)
+        {
+            GameObject temp = Instantiate(_MusicPrefab);
+            DontDestroyOnLoad(temp);
+        }
+	}
+}
